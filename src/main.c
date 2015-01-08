e3777cd0041cb9353bdd9f0a55f749857712340f
Kevin James <KevinJames@thekev.in>
2015-01-08 11:57:42 -0500

    add printf

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include <LPC17xx.h>
+#include "printf.h"
+#include "stdefs.h"
 #include "uart_polling.h"
+
+void putc(void* p, char c) {
+    uart_put_char(0, c);
+}
 
 int main() {
     SystemInit();
     uart0_init();
+    uart0_put_string("------------\n\r");
     uart0_put_string("Hello World!\n\r");
+	
+	  init_printf(NULL, putc);
+    printf("Hello from printf!\n\r");
     return 0;
 }
---
+++ src/stdefs.h
@@ -0,0 +1,6 @@
+#ifndef STDEFS_H
+#define STDEFS_H
+
+#define NULL 0
+
+#endif
